4fe3cef0fd8fcd612ee3e6342b5c635c830e3eb9
Gabrielbprado <guardavege@gmail.com>
2024-04-23 08:59:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponentComponent } from '../core/navbar/navbar-component/navbar-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
